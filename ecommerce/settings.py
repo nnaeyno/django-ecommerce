64d8446349479cc0c504ae9b34a41c472dc26cec
nnaeyno <ngogo20@freeuni.edu.ge>
2024-10-24 02:02:38 +0400

    tag updates in html

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 'store.context_processors.parent_categories',
+                'store.context_processors.product_tags',
             ],
         },
     },
--- store/context_processors.py
+++ store/context_processors.py
@@ -1,6 +1,11 @@
-from store.models import Category
+from store.models import Category, Product
 
 
 def parent_categories(request):
     parents = Category.objects.filter(parent__isnull=True)
     return {'parent_categories': parents}
+
+
+def product_tags(request):
+    tags = Product.objects.all()
+    return {'product_tags': tags}
